f8e1ead1c7e4f66ad890390b48c6d215738b8199
Harveer Singh <hsingh@mozilla.com>
2026-03-29 08:14:56 +0000

    Bug 2021894 - Validate DatabaseFile actor manager chain in VerifyRequestParams to reject cross-database actor references. a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D287604
    
    Differential Revision: https://phabricator.services.mozilla.com/D290134

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -10329,6 +10329,13 @@ bool TransactionBase::VerifyRequestParams(
         if (NS_AUUF_OR_WARN_IF(!file)) {
           return false;
         }
+
+        // Reject actors managed by a different Database
+        if (NS_AUUF_OR_WARN_IF(file->Manager() !=
+                               static_cast<const PBackgroundIDBDatabaseParent*>(
+                                   &GetDatabase()))) {
+          return false;
+        }
         break;
 
       case StructuredCloneFileBase::eMutableFile: {
