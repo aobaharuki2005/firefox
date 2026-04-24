8d2cdef84bc6e27610548af6fe4fbafef456b644
Paul Adenot <paul@paul.cx>
2026-04-24 14:27:14 +0000

    Bug 2029700  a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D293962

--- dom/media/MediaData.cpp
+++ dom/media/MediaData.cpp
@@ -104,13 +104,18 @@ bool AudioData::SetTrimWindow(const media::TimeInterval& aTrim) {
   }
 
   size_t frameOffset = trimBefore.ToTicksAtRate(mRate);
+  size_t dataOffset = frameOffset * mChannels;
+  int64_t frameCountAfterTrim = (trimAfter - trimBefore).ToTicksAtRate(mRate);
+  if (dataOffset > mAudioData.Length() || frameCountAfterTrim < 0) {
+    return false;
+  }
   mTrimWindow = Some(aTrim);
-  mDataOffset = frameOffset * mChannels;
-  MOZ_DIAGNOSTIC_ASSERT(mDataOffset <= mAudioData.Length(),
-                        "Data offset outside original buffer");
-  mFrames = (trimAfter - trimBefore).ToTicksAtRate(mRate);
-  MOZ_DIAGNOSTIC_ASSERT(mFrames <= mAudioData.Length() / mChannels,
-                        "More frames than found in container");
+  mDataOffset = dataOffset;
+  const size_t availFrames = (mAudioData.Length() - mDataOffset) / mChannels;
+  if (frameCountAfterTrim > AssertedCast<int64_t>(availFrames)) {
+    frameCountAfterTrim = AssertedCast<int64_t>(availFrames);
+  }
+  mFrames = AssertedCast<uint32_t>(frameCountAfterTrim);
   mTime = mOriginalTime + trimBefore;
   mDuration = TimeUnit(mFrames, mRate);
 
