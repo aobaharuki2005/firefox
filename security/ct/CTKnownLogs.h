cbece2aacbd53a67eaf582f13ac125594739e4f4
ffxbld <ffxbld@mozilla.com>
2026-05-25 17:39:00 +0000

    No Bug, mozilla-esr140 repo-update HSTS HPKP remote-settings ct-logs -  a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D302300

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1785146865000000);
+static const PRTime kCTExpirationTime = INT64_C(1785753070000000);
 
 namespace mozilla::ct {
 
